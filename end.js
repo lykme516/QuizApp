8f3bd7a0d42750080da5f10afc2f9c4a9459c6f7
lykme516 <sjung516@gmail.com>
2020-08-29 09:22:13 -0400

    Completed the basic quiz exercise.

---
+++ end.js
@@ -0,0 +1,35 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    console.log(username.value);
+});
+
+saveHighScore = (e) => {
+    console.log("clicked the save button");
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort( (a,b) => {
+        return b.score - a.score;
+    })
+
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    console.log(highScores);
+
+}
